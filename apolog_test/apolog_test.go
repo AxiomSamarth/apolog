06e851302e072a387b9e63cfc3d3f89368ae226d
samarthdeyagond <samarth.deyagond@plexxi.com>
2020-06-01 22:14:26 +0530

    Quick structure modification

---
+++ apolog_test/apolog_test.go
@@ -0,0 +1,37 @@
+package apolog_test
+
+import (
+	"testing"
+
+	"../apolog"
+)
+
+// TestInfo function tests Info level of logging
+func TestInfo(t *testing.T) {
+	var data = []byte(`Log this!`)
+	apolog.Info(data)
+}
+
+// TestError function tests Info level of logging
+func TestError(t *testing.T) {
+	var data = []byte(`Log this!`)
+	apolog.Error(data)
+}
+
+// TestWarn function tests Info level of logging
+func TestWarn(t *testing.T) {
+	var data = []byte(`Log this!`)
+	apolog.Warn(data)
+}
+
+// TestDebug function tests Info level of logging
+func TestDebug(t *testing.T) {
+	var data = []byte(`Log this!`)
+	apolog.Debug(data)
+}
+
+// TestTrace function tests Info level of logging
+func TestTrace(t *testing.T) {
+	var data = []byte(`Log this!`)
+	apolog.Trace(data)
+}
